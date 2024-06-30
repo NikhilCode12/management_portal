c0b7d94474b35127da2a2d3596deccea1f17daf1
NikhilCode12 <promaxchessace@gmail.com>
2024-06-30 22:11:47 +0530

    update: scrollable content

--- app/components/StudentListModal.jsx
+++ app/components/StudentListModal.jsx
@@ -53,7 +53,14 @@ const StudentListModal = ({ onSelect, onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
-      <div className="w-3/4 bg-white rounded-lg shadow-lg relative">
+      <div className="w-3/4  bg-white rounded-lg shadow-lg relative ">
+        <style>
+          {`
+            .scrollbar-hidden::-webkit-scrollbar {
+              display: none;
+            }
+          `}
+        </style>
         <div className="p-6">
           <div className="flex justify-between items-center">
             <h2 className="text-2xl font-bold mb-4">
@@ -88,43 +95,47 @@ const StudentListModal = ({ onSelect, onClose }) => {
               </p>
             </div>
           ) : data && data.length > 0 ? (
-            <table className="min-w-full bg-white border border-gray-300">
-              <thead>
-                <tr>
-                  <th className="px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-900 uppercase tracking-wider">
-                    Application Number
-                  </th>
-                  <th className="px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-900 uppercase tracking-wider">
-                    Candidate Full Name
-                  </th>
-                  <th className="px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-900 uppercase tracking-wider">
-                    Option
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {data.map((student) => (
-                  <tr key={student.applicationNumber}>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      {student.applicationNumber}
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      {capitalize(student.first_name)}{" "}
-                      {capitalize(student.middle_name)}{" "}
-                      {capitalize(student.surname)}
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                      <button
-                        className="bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-600 text-white px-4 py-2 font-semibold rounded-md"
-                        onClick={() => handleSelect(student.applicationNumber)}
-                      >
-                        Select
-                      </button>
-                    </td>
+            <div className="max-h-[50vh] overflow-y-auto scrollbar-hidden">
+              <table className="min-w-full bg-white border border-gray-300">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-900 uppercase tracking-wider">
+                      Application Number
+                    </th>
+                    <th className="px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-900 uppercase tracking-wider">
+                      Candidate Full Name
+                    </th>
+                    <th className="px-6 py-3 border-b border-gray-300 bg-gray-100 text-left text-sm leading-4 font-medium text-gray-900 uppercase tracking-wider">
+                      Option
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {data.map((student) => (
+                    <tr key={student.applicationNumber}>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        {student.applicationNumber}
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        {capitalize(student.first_name)}{" "}
+                        {capitalize(student.middle_name)}{" "}
+                        {capitalize(student.surname)}
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <button
+                          className="bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-600 text-white px-4 py-2 font-semibold rounded-md"
+                          onClick={() =>
+                            handleSelect(student.applicationNumber)
+                          }
+                        >
+                          Select
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           ) : (
             <div className="flex items-center justify-center h-full">
               <p className="text-lg font-semibold">No student records found</p>
